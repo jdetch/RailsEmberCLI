dcbfec6e0f027e0d2e500d833925cb5cb1bb8ea0
Jodi Detch <jodidetch@gmail.com>
2014-09-30 16:32:37 -0400

    API is configured to connect to front-end application via CORS

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
+require "rails/all"
 require "active_model/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
@@ -15,6 +16,14 @@ Bundler.require(*Rails.groups)
 
 module Api
   class Application < Rails::Application
+    config.middleware.use Rack::Cors do
+        allow do
+            origins "*"
+            resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]
+            end
+        end
+  end
+end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -26,5 +35,4 @@ module Api
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  end
-end
+
